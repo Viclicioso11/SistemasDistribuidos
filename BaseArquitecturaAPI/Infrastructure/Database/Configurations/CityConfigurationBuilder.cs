2337c4b50ff82c73488e8704ab7a199a4d7217bf
jonathanbonilla30 <jonathan.bonilla@est.uca.edu.ni>
2020-09-24 17:23:53 -0600

    Probando rama

--- BaseArquitecturaAPI/Infrastructure/Database/Configurations/CityConfigurationBuilder.cs
+++ BaseArquitecturaAPI/Infrastructure/Database/Configurations/CityConfigurationBuilder.cs
@@ -40,7 +40,13 @@ namespace Infrastructure.Database.Configurations
 
             builder
                 .HasData(
-                new City { CityId = 1, CityName = "Boaco", CityCode = "361", StateId = 1 });
+                new City { CityId = 1, CityName = "Boaco", CityCode = "361", StateId = 1 },
+                new City { CityId = 2, CityName = "Camoapa", CityCode = "362", StateId = 1 },
+                new City { CityId = 3, CityName = "Santa Lucía", CityCode = "363", StateId = 1 },
+                new City { CityId = 4, CityName = "San José del Remate", CityCode = "364", StateId = 1 },
+                new City { CityId = 5, CityName = "San Lorenzo", CityCode = "365", StateId = 1 },
+                new City { CityId = 6, CityName = "Teustepe", CityCode = "366", StateId = 1 }
+                );
         }
-    }
+    }                       
 }
